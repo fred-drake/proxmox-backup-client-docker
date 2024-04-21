09ea3e0f8b43cdd0df519a5e49a0cdc81153c940
Fred Drake <fred.drake@gmail.com>
2024-04-21 15:23:04 -0400

    Fixed env issue on cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cleanup() {
-  echo "Exiting..."
-  exit 0
+	echo "Exiting..."
+	exit 0
 }
 
 trap cleanup SIGINT
@@ -10,17 +10,18 @@ trap cleanup SIGTERM
 
 # Exit if BACKUP_TARGETS is not set
 if [ -z "$BACKUP_TARGETS" ]; then
-  echo "BACKUP_TARGETS is not set. Set in the form of 'root.pxar:/' or several separated by space, like 'etc.pxar:/etc var.pxar:/var'. Exiting."
-  exit 1
+	echo "BACKUP_TARGETS is not set. Set in the form of 'root.pxar:/' or several separated by space, like 'etc.pxar:/etc var.pxar:/var'. Exiting."
+	exit 1
 fi
 
 # Exit if no CRON_SCHEDULE is set
 if [ -z "$CRON_SCHEDULE" ]; then
-  echo "CRON_SCHEDULE is not set. Set in the form of Cron timing, e.g. '00 03 * * *' for 3am every day.  Exiting."
-  exit 1
+	echo "CRON_SCHEDULE is not set. Set in the form of Cron timing, e.g. '00 03 * * *' for 3am every day.  Exiting."
+	exit 1
 fi
 
-echo "$CRON_SCHEDULE /do_backup.sh > /proc/1/fd/1 2>&1" > /cronfile
+env > /cronfile
+echo "$CRON_SCHEDULE /do_backup.sh > /proc/1/fd/1 2>&1" >> /cronfile
 crontab /cronfile
 rm -f /cronfile
 cron
@@ -31,5 +32,5 @@ echo "Starting backup service..."
 
 # Loop forever
 while true; do
-    sleep 1
+	sleep 1
 done
